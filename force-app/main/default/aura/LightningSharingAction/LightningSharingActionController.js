caf801546e8c1d2405d522035cf39d33ceafa956
Jean-Noel <jeannoel.casassus@gmail.com>
2020-11-05 11:51:48 +0000

    Default sharing model, sharing as popup

--- force-app/main/default/aura/LightningSharingAction/LightningSharingActionController.js
+++ force-app/main/default/aura/LightningSharingAction/LightningSharingActionController.js
@@ -1,13 +1,28 @@
 ({
     doInit: function (component) {
-        component.find('navService').navigate({
-            type: 'standard__component',
-            attributes: {
-                componentName: 'c__LightningSharing'
+        $A.createComponent(
+            'c:LightningSharing',
+            {
+                recordId: component.get('v.recordId')
             },
-            state: {
-                c__recordId: component.get('v.recordId')
+            function (content, status, errorMessage) {
+                if (status === 'SUCCESS') {
+                    var modalBody = content;
+                    component
+                        .find('modalService')
+                        .showCustomModal({
+                            //header: 'Sharing',
+                            body: modalBody,
+                            showCloseButton: false,
+                            cssClass: 'fix-min-height slds-modal_large',
+                            closeCallback: function (ovl) {}
+                        })
+                        .then(function (overlay) {});
+                } else if (status === 'ERROR') {
+                    console.log('Error: ' + errorMessage);
+                    // Show error message
+                }
             }
-        });
+        );
     }
 });
